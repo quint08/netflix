366568cad292b643c4108643c3e9d01fbf072b77
Quinten <531464@student.glu.nl>
2020-02-10 14:04:40 +0100

    netflix gitignore fix

---
+++ build/precache-manifest.905eead7f5394c7f69a3e3b6db17241b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdd8cac6dc45c13d11303e44dae19a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c67fb8baf79cdd2ef17",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "d70d4c47dc0216e525ad",
+    "url": "/static/css/main.be3e5f1c.chunk.css"
+  },
+  {
+    "revision": "7c67fb8baf79cdd2ef17",
+    "url": "/static/js/2.0918c2e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0918c2e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d70d4c47dc0216e525ad",
+    "url": "/static/js/main.7ee2d174.chunk.js"
+  },
+  {
+    "revision": "58c4bf85b57b151eca7f",
+    "url": "/static/js/runtime-main.1f1645e7.js"
+  },
+  {
+    "revision": "2ea85d7448475a744c1485c2eac3d3d1",
+    "url": "/static/media/logo.2ea85d74.png"
+  }
+]);
